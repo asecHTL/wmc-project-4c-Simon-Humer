69e332d3afb36d9406137a75b2e88af1e1050a7d
Simon Humer <HumerS22022@sus.htl-grieskirchen.at>
2026-06-02 19:29:14 +0200

    Added div fields and added task description... to personal task view

--- Frontend/taskflowlive/src/routes/tasks/+page.svelte
+++ Frontend/taskflowlive/src/routes/tasks/+page.svelte
@@ -1,44 +1,97 @@
 <script>
+    import { userData } from "$lib/shared/User.svelte";
+
     let { data } = $props();
+
+    async function addTask() {}
+
+    let counterToDo = $state(0);
+    let counterInProgress = $state(0);
+    let counterReview = $state(0);
+    let counterDone = $state(0);
 </script>
 
+<div class="header">
+    <div class="filter">Filter</div>
+    <div class="groupBy">Group By</div>
+    <div class="sortBy">Sort by</div>
+
+    <div class="searchBar">Search taks..</div>
+
+   
+        <button class="addTask" onclick={() => addTask()}>Add Task</button>
+    
+</div>
+
 <div class="toDo">
-    <h2>ToDo</h2>
+    <div class="headerStatus"><h2>ToDo {counterToDo}</h2></div>
 
     <ul>
         {#each data.tasks as task}
             {#if task.taskStatus === "toDo"}
-                <li>{task.taskTitle}</li>
+                <div class="taskCard">
+                    <div class="taskCardTitle">{task.taskTitle}</div>
+                    <div class="taskCardDescription">
+                        {task.taskDescription}
+                    </div>
+                    <div class="taskCardPriority">{task.taskPriority}</div>
+                    <div class="taskEndDate">{task.taskEndDate}</div>
+                    <div class="taskProjectName">Project xy</div>
+                </div>
             {/if}
         {/each}
     </ul>
 </div>
 <div class="inProgress">
-    <h2>In Progress</h2>
+    <div class="headerStatus"><h2>In Progress {counterInProgress}</h2></div>
     <ul>
         {#each data.tasks as task}
             {#if task.taskStatus === "inProgress"}
-                <li>{task.taskTitle}</li>
+                <div class="taskCard">
+                    <div class="taskCardTitle">{task.taskTitle}</div>
+                    <div class="taskCardDescription">
+                        {task.taskDescription}
+                    </div>
+                    <div class="taskCardPriority">{task.taskPriority}</div>
+                    <div class="taskEndDate">{task.taskEndDate}</div>
+                    <div class="taskProjectName">Project xy</div>
+                </div>
             {/if}
         {/each}
     </ul>
 </div>
 <div class="review">
-    <h2>Review</h2>
+    <div class="headerStatus"><h2>Review {counterReview}</h2></div>
     <ul>
         {#each data.tasks as task}
             {#if task.taskStatus === "review"}
-                <li>{task.taskTitle}</li>
+                <div class="taskCard">
+                    <div class="taskCardTitle">{task.taskTitle}</div>
+                    <div class="taskCardDescription">
+                        {task.taskDescription}
+                    </div>
+                    <div class="taskCardPriority">{task.taskPriority}</div>
+                    <div class="taskEndDate">{task.taskEndDate}</div>
+                    <div class="taskProjectName">Project xy</div>
+                </div>
             {/if}
         {/each}
     </ul>
 </div>
 <div class="done">
-    <h2>Done</h2>
+    <div class="headerStatus"><h2>Done {counterDone}</h2></div>
     <ul>
         {#each data.tasks as task}
             {#if task.taskStatus === "done"}
-                <li>{task.taskTitle}</li>
+                <div class="taskCard">
+                    <div class="taskCardTitle">{task.taskTitle}</div>
+                    <div class="taskCardDescription">
+                        {task.taskDescription}
+                    </div>
+                    <div class="taskCardPriority">{task.taskPriority}</div>
+                    <div class="taskEndDate">{task.taskEndDate}</div>
+                    <div class="taskProjectName">Project xy</div>
+                </div>
             {/if}
         {/each}
     </ul>
